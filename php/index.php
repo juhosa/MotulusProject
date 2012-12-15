c900f2bfb088a81824f1855ebe3cfbe12fe847c3
ISO-B <arctice@gmail.com>
2012-12-15 11:41:24 +0200

    Update php/index.php

--- php/index.php
+++ php/index.php
@@ -2,8 +2,35 @@
 /*
 +--------------------------------------------------------------------------
 |
-|  
+|  Tänne tulee suurinosa koodista
 |
 +--------------------------------------------------------------------------
 */
 
+class Motulus {
+	private $utils;
+
+	function Motulus() {
+		require "utils.php";
+		$this->utils = new utils();
+		$this->utils->GetUserdate();
+
+		if($this->utils->CurrentUser == null) {
+			$this->PageLogin();
+		}else{
+			switch($utils->input['op']) {
+				case 'hops':
+					$this->PageHops();
+					break;
+				case 'logout':
+					$this->ActionLogout();
+					break;
+				default:
+					$this->PageFrontpage();
+					break;
+			}
+		}
+	}
+}
+
+new Motulus();
